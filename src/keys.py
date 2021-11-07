7ebde95fcd35de38bc9f24031143267e1b3d8b5f
Phil Abercrombie <phil@abercrombie-family.org>
2021-11-06 23:11:55 -0400

    keys.py : package for managing tft disply pushbutton switches

---
+++ src/keys.py
@@ -0,0 +1,74 @@
+import threading
+import logging
+from pprint import pprint as pp
+import RPi.GPIO as GPIO
+import time
+
+class Keys:
+    PRESS = 1
+
+    __DEBOUNCE_TIME = 0.2
+    __REPEAT_INTERVAL = 1
+
+    def __init__(self):
+        self.__handlers={}
+        GPIO.setmode(GPIO.BCM)
+        self.__bcm = {
+            27: {'index':0, 'bcm':27, 'callback':lambda key: 0, 'repeat_timer': None, 'repeat_count': 0, 'state':0, 'clicktime': 0},
+            23: {'index':1, 'bcm':23, 'callback':lambda key: 0, 'repeat_timer': None, 'repeat_count': 0, 'state':0, 'clicktime': 0},
+            22: {'index':2, 'bcm':22, 'callback':lambda key: 0, 'repeat_timer': None, 'repeat_count': 0, 'state':0, 'clicktime': 0},
+            17: {'index':3, 'bcm':17, 'callback':lambda key: 0, 'repeat_timer': None, 'repeat_count': 0, 'state':0, 'clicktime': 0}}
+
+        for key in self.__bcm.values():
+            GPIO.setup(key['bcm'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(key['bcm'], GPIO.BOTH)
+            GPIO.add_event_callback(key['bcm'], self.__detect_edge)
+
+    def __autorepeat(self, bcm):
+        key = self.__bcm[bcm]
+        if GPIO.input(bcm) == 0: # it is still down
+            key['repeat_count'] += 1
+            key.get('callback', lambda:0)(key)
+            key['repeat_timer'] = threading.Timer(Keys.__REPEAT_INTERVAL, lambda : self.__autorepeat(bcm))
+            key['repeat_timer'].start()
+
+    def __detect_edge(self, bcm):
+        key = self.__bcm[bcm]
+        if time.time() - key['clicktime'] < Keys.__DEBOUNCE_TIME:
+            return
+
+        if GPIO.input(key['bcm']) == 0: # it was a button press
+            if key['repeat_timer']:
+                key['repeat_timer'].cancel()
+            key['clicktime'] = time.time()
+            key['repeat_count'] = 0
+            key.get('callback', lambda:0)(key)
+            key['repeat_timer'] = threading.Timer(Keys.__REPEAT_INTERVAL, lambda : self.__autorepeat(bcm))
+            key['repeat_timer'].start()
+        else: # it was a button release
+            if key['repeat_timer']:
+                key['repeat_timer'].cancel()
+
+    def set_handler(self, index, callback):
+        for key in self.__bcm.values():
+            if key['index'] == index:
+                key['callback'] = callback
+
+    def close():
+        print("CLOSE")
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    input = Keys()
+    handle_key = lambda key: pp(key)
+
+    input.set_handler(0, handle_key)        
+    input.set_handler(1, handle_key)        
+    input.set_handler(2, handle_key)        
+    input.set_handler(3, handle_key)        
+
+    print("Press ANY key")
+    time.sleep(100)
