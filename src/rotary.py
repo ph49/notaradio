bffe5ffe8830acacfa7ec918ddd36a3c462850b1
Phil Abercrombie <phil@abercrombie-family.org>
2023-03-12 13:39:39 -0400

    Implementation of rotary encoder

--- src/rotary.py
+++ src/rotary.py
@@ -5,40 +5,87 @@ import RPi.GPIO as GPIO
 import time
 
 class Rotary:
-    PRESS = 1
-
-    _DEBOUNCE_TIME = 0.15
-
-    GPIO.setmode(GPIO.BCM)
     __bcm = {}
 
-    def set_gpio_handler(bcm_a, bcm_b, callback, detail=None):
-        GPIO.setup(bcm, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(bcm, GPIO.BOTH)
-        GPIO.add_event_callback(bcm, Rotary.__detect_edge)
-        Rotary.__bcm[bcm] = {
-            'bcm': bcm,
+    # TRANSITIONS [which-gpio][state][which-direction 0/1] = (direction, new-state)
+    TRANSITIONS = [
+        [ # Edge on GPIO A
+            [ # Current state is 0b00
+                None, 
+                (+1, 0b01)
+            ],
+            [ # Current state is 0b01 
+                (-1, 0b00),
+                None
+            ],
+            [ # Current state is 0b10
+                None,
+                (-1, 0b11)
+            ],
+            [ # Current state is 0b11 
+                (+1, 0b10),
+                None
+            ],
+        ],
+        [ # Edge on GPIO B
+            [ # Current state is 0b00
+                None, 
+                (-1, 0b10)
+            ],
+            [ # Current state is 0b01 
+                None,
+                (+1, 0b11)
+            ],
+            [ # Current state is 0b10
+                (+1, 0b00),
+                None
+            ],
+            [ # Current state is 0b11 
+                (-1, 0b01),
+                None
+            ],
+        ],
+    ]
+
+        
+
+    GPIO.setmode(GPIO.BCM)
+    def set_gpio_rotary_handler(bcm_a, bcm_b, callback, detail=None):
+        # state is 2-bit encoding of state of bcm_b, bcm_a
+        handler = {
+            'state' : 0,
             'callback': callback,
             'detail': detail
         }
 
+        for idx, bcm in enumerate([bcm_a, bcm_b]):
+            GPIO.setup(bcm, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(bcm, GPIO.BOTH)
+            GPIO.add_event_callback(bcm, Rotary.__detect_edge)
+            Rotary.__bcm[bcm] = {
+                'handler': handler,
+                'index': idx,
+                'tx': Rotary.TRANSITIONS[idx]
+            }
+
+
     def __detect_edge(bcm):
+        edge = 1 if GPIO.input(bcm) else 0
         key = Rotary.__bcm[bcm]
-        if time.time() - key['clicktime'] < Rotary._DEBOUNCE_TIME:
-            return
-
-        key['clicktime'] = time.time()
-        key.get('callback', lambda:0)(key['detail'])
-
-    def close():
-        print("CLOSE")
+        state = key['handler']['state']
+#        print(f'{bcm} {edge} {state}')
+        tx = Rotary.TRANSITIONS[key['index']][state][edge]
+        if tx:
+#            print(f'{tx[0]} {state}->{tx[1]}')
+            key['handler']['state'] = tx[1]    
+            key['handler']['callback'](tx[0])
+        else:
+#            print("XXX")
+            pass
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
-    Rotary.set_gpio_handler(17, lambda x: print(x), detail='KEY 0 (BCM=17)')
-    Rotary.set_gpio_handler(22, lambda x: print(x), detail='KEY 1 (BCM=22)')
-    Rotary.set_gpio_handler(23, lambda x: print(x), detail='KEY 2 (BCM=23)')
-    Rotary.set_gpio_handler(27, lambda x: print(x), detail='KEY 3 (BCM=27)')
-    print("Press Rotary")
+    print("Twiddle Knob")
+    Rotary.set_gpio_rotary_handler(5, 6, print, detail='Rotary')
     time.sleep(100)
